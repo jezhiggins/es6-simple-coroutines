3e23252bb218f46b8f36586a9482749dd145fcd4
Jez Higgins <jez@jezuk.co.uk>
2017-04-28 15:54:44 +0100

    got rid of task class. reoroder for diadactic effect

--- coroutines.js
+++ coroutines.js
@@ -1,3 +1,9 @@
+function* async_search(iterable, async_predicate) {
+    for (const item of iterable)
+	if (yield* async_predicate(item))
+	    return item;
+    return null;
+} // async_search
 
 function* lucas_sequence() {
     let a = 2, b = 1;
@@ -11,7 +17,7 @@ function* lucas_sequence() {
 function* async_is_prime(x) {
     if (x < 2)
 	return false;
-    for (let i = 2; i != Math.sqrt(x) + 1; ++i) {
+    for (const i of int_range(2, Math.sqrt(x) + 1)) {
 	if (x % i == 0)
 	    return false;
 	yield* async_sleep(0);
@@ -19,12 +25,10 @@ function* async_is_prime(x) {
     return true;
 } // async_is_prime
 
-function* async_search(iterable, async_predicate) {
-    for (const item of iterable)
-	if (yield* async_predicate(item))
-	    return item;
-    return null;
-} // async_search
+function* int_range(from, to) {
+    for (let i = from; i <= to; ++i)
+	yield i;
+} // int_range
 
 function* async_print_matches(iterable, async_predicate) {
     for (const item of iterable) {
@@ -33,12 +37,6 @@ function* async_print_matches(iterable, async_predicate) {
 	    console.log(`Found ${item}`)
     }
 } // async_print_matches
-
-function* async_message_after(message, interval_seconds) {
-    yield* async_sleep(interval_seconds);
-    console.log(message);
-    return `I said "${message}"`;
-} // async_message_after
 
 function* async_repetitive_message(message, interval_seconds) {
     while (true) {
@@ -57,37 +55,22 @@ function* async_sleep(interval_seconds) {
     }
 } // async_sleep
 
+function* async_message_after(message, interval_seconds) {
+    yield* async_sleep(interval_seconds);
+    console.log(message);
+    return `I said "${message}"`;
+} // async_message_after
 
 ///////////////////////////////////
-class task {
-    constructor(iterable) {
-	this.iterable_ = iterable;
-	this.id = task.next_id.next().value;
-    }
-
-    next() {
-	return this.iterable_.next();
-    } // next
-
-    return() {
-	return this.iterable_.return();
-    } // return
-}
-task.next_id_gen = function*() {
-    let id = 0;
-    while (true)
-	yield id++;
-} // next_id_gen
-task.next_id = task.next_id_gen();
-
 class scheduler {
     constructor() {
+	this.id_ = 0;
 	this.tasks_ = []
 	this.monitor_ = null;
     }
 
     add_task(generator) {
-	this.tasks_.push(new task(generator));
+	this.tasks_.push({id: this.id_++, worker: generator});
     }
     set_monitor(generator) {
 	this.monitor_ = generator;
@@ -98,11 +81,12 @@ class scheduler {
 	while (this.tasks_.length != 0) {
 	    const task = this.tasks_[0];
 	    this.tasks_ = this.tasks_.slice(1);
-	    const step = !done ? task.next() : task.return();
+
+	    const step = !done ? task.worker.next() : task.worker.return();
 	    if (step.done)
 		console.log(`Task ${task.id} completed with exit value: ${step.value}`);
 	    else
-		this.tasks_ = this.tasks_.concat([task]);
+		this.tasks_.push(task);
 
 	    if (this.monitor_)
 		done = this.monitor_.next().done;
@@ -113,5 +97,6 @@ class scheduler {
 const loop = new scheduler();
 loop.add_task(async_repetitive_message("Unexpected item in the bagging area", 2));
 loop.add_task(async_print_matches(lucas_sequence(), async_is_prime));
+loop.add_task(async_message_after("GETTING A BIT BORED NOW", 18));
 loop.set_monitor(async_message_after("SCREW THIS!", 25));
 loop.run();
